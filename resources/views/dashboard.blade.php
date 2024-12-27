b7562e6c007a63fc44a7718a601f1abe0103569e
Angus <angus.goody@gmail.com>
2024-12-27 14:11:22 +0000

    tweaked ui

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,6 +23,64 @@
                 </p>
                 <div class="divider"></div>
             </div>
+
+            <!-- Time picker -->
+            <div
+                   class="relative flex flex-col mx-auto items-center justify-center p-8 bg-base-100 rounded-lg  w-full lg:w-3/4 max-w-2xl"
+               >
+                   <div class="flex items-center space-x-6">
+                       <!-- Hour Selector -->
+                       <div class="flex flex-col items-center">
+                           <button
+                               class="text-2xl text-gray-600 hover:text-gray-800 focus:outline-none"
+                               @click="incrementHours"
+                           >
+                               &#9650;
+                           </button>
+                           <input
+                               type="text"
+                               class="w-24 text-center text-2xl input input-lg "
+                               v-model="hours"
+                               @input="manualUpdate"
+                           />
+                           <button
+                               class="text-2xl text-gray-600 hover:text-gray-800 focus:outline-none"
+                               @click="decrementHours"
+                           >
+                               &#9660;
+                           </button>
+                       </div>
+
+                       <span class="text-3xl">:</span>
+
+                       <!-- Minute Selector -->
+                       <div class="flex flex-col items-center">
+                           <button
+                               class="text-2xl text-gray-600 hover:text-gray-800 focus:outline-none"
+                               @click="incrementMinutes"
+                           >
+                               &#9650;
+                           </button>
+                           <input
+                               type="text"
+                               class="w-24 text-center text-2xl input input-lg "
+                               v-model="minutes"
+                               @input="manualUpdate"
+                           />
+                           <button
+                               class="text-2xl text-gray-600 hover:text-gray-800 focus:outline-none"
+                               @click="decrementMinutes"
+                           >
+                               &#9660;
+                           </button>
+                       </div>
+                   </div>
+
+
+                <button class="btn btn-square btn-primary btn-soft absolute right-4 top-1/2 -translate-y-1/2" aria-label="Icon Button">
+                    <span class="icon-[tabler--refresh]"></span>
+                </button>
+               </div>
         </div>
     </div>
 </x-app-layout>
